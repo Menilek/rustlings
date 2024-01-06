8de65011d7456c2f9dd6ec7c50044d3710282dfd
Menilek Techane <menilek25@hotmail.co.uk>
2024-01-06 09:01:30 +0000

    Complete intro exercises

---
+++ exercises/intro/intro2.rs
@@ -0,0 +1,12 @@
+// intro2.rs
+//
+// Make the code print a greeting to the world.
+//
+// Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
+// hint.
+
+
+fn main() {
+    let word = String::from("World");
+    println!("Hello {word}!");
+}
